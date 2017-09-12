f4e8db91c22b93f6e29ace8ebc7671c04a261fe4
carlos do absurdo <vytallima@gmail.com>
2017-09-12 10:45:58 -0300

    teste

---
+++ assets/src/components/A.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h2 class="red">{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+import style from '../styles/A.styl'
+
+export default {
+  data () {
+    return {
+      msg: 'comp a'
+    }
+  }
+}
+</script>
